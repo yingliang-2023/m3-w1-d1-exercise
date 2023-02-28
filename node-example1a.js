f91c124efde80610f566e40d2df44c746c95f4ac
YING LIANG <y.liang.168@westcliff.edu>
2023-02-27 22:18:54 -0600

    end

--- node-example1a.js
+++ node-example1a.js
@@ -3,3 +3,4 @@ var myLogModule = require('./node-example1.js'); // add ./ because module is in 
 myLogModule.info('Node.js started');
 myLogModule.warning('Node.js has a warning');
 myLogModule.error('Node.js has an error');
+
